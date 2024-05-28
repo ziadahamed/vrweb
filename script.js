14200897fedd92cbbf1575575278aaafdadb64de
ziadahamed <151837336+ziadahamed@users.noreply.github.com>
2024-05-28 11:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var popupover = document.querySelector(".popup-over");
+var popup =document.querySelector(".popup");
+var but = document.getElementById("btn");
+
+
+but.addEventListener("click",function(){
+
+    popupover.style.display = "block";
+    popup.style.display ="block";
+
+})
+
+
